e289ac979bb11c6cef53eec64358c70255d32c61
Parth Patel <parthspatel.nj@gmail.com>
2018-01-22 23:54:38 -0500

    Add files via upload

---
+++ Modules/probe.py
@@ -0,0 +1,109 @@
+import pandas as pd
+
+from Bio import SeqIO
+from collections import Counter
+
+class probe:
+
+    dictDNA = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+    degenerate = ['R', 'Y', 'M', 'K', 'S', 'W', 'H', 'B', 'V', 'D', 'N']
+
+
+    def __init__(self, paths, length = 23, frac = 0.001):
+        assert frac <= 1 and frac >= 0, 'Frac must be between 0 and 1 inclusively'
+        try:
+            for path in paths:
+                f = open(path)
+                f.close()
+        except IOError:
+            print('File is not accessible')
+
+        self.paths = paths
+        self.length = length
+        self.frac = frac
+
+
+    def importSequences(self):
+        seqs = []
+        for path in self.paths:
+            with open(path) as handle:
+                seqs += list(SeqIO.parse(handle, 'fasta'))
+        self.seqs = seqs
+
+        return self
+
+
+    # Generate the kmer sequnces from the fasta input
+    def generate(self):
+        rmers = Counter()
+        for seq in self.seqs:
+            seqStr = str(seq.seq.ungap('-').upper())
+            lenth = len(seqStr)
+
+            for loc in range(len(seqStr)-self.length):
+                rmers[seqStr[loc:loc+self.length]] += 1
+
+        self.filterRare(rmers)
+        self.filterPAM().filterDegenerate().convertPAM().extractProto()
+        self.kmers = pd.DataFrame(self.kmers)
+
+        return self
+
+
+    # Remove Sequences that have low occurances (rare)
+    def filterRare(self, rmers):
+        cutoff = len(self.seqs) * self.frac
+        nonRare = sum(count > cutoff for count in rmers.values())
+        self.kmers = pd.Series([seq for seq, count in rmers.items() if count > cutoff])
+
+        return self
+
+
+    # Return the reverse compliment of the input sequence
+    def reverseCompliment(self, seq):
+        return ''.join([self.dictDNA[base] for base in list(seq[::-1])])
+
+
+    # Convert any seqs starting in CC to their proper PAM seqs
+    def convertPAM(self):
+        self.kmers = [list(k) for k in self.kmers.values]
+        for i,k in enumerate(self.kmers):
+            l = ''.join(k)
+            if l.startswith('CC'):
+                self.kmers[i] = self.reverseCompliment(l)
+            else:
+                self.kmers[i] = l
+
+        return self
+
+
+    # Filter out any seqs that do no end or start with PAM or it's compliment
+    def filterPAM(self):
+        self.kmers = self.kmers[self.kmers.map(lambda x: x.endswith('GG') | x.startswith('CC'))]
+        self.kmers.reset_index()
+        return self
+
+
+    # Filter out any seqs that contain degenerate bases
+    def filterDegenerate(self):
+        self.kmers = self.kmers[self.kmers.map(lambda x: all(b not in x for b in self.degenerate))]
+        self.kmers.reset_index()
+        return self
+
+
+    # Split the protospacer from the PAM seqs
+    def extractProto(self):
+        pam = [0] * len(self.kmers)
+        proto = [0] * len(self.kmers)
+        for i,p in enumerate(self.kmers):
+            l = ''.join(p)
+            j = l[-3:]
+            k = l[0 : self.length - 3]
+
+            pam[i] = j
+            proto[i] = k
+
+        self.pam = pd.DataFrame(pam)
+        self.proto = pd.DataFrame(proto)
+
+        return self
---
+++ Modules/target.py
@@ -0,0 +1,94 @@
+import pandas as pd
+
+from probe import probe
+from Bio import SeqIO
+from collections import Counter
+
+class target:
+
+    dictDNA = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G', '-': '-'}
+    degenerate = ['R', 'Y', 'M', 'K', 'S', 'W', 'H', 'B', 'V', 'D', 'N']
+
+    def __init__(self, data, paths, length = 23):
+        try:
+            for path in paths:
+                f = open(path)
+                f.close()
+        except IOError:
+            print('File is not accessible')
+
+        self.paths = paths
+        self.length = length
+
+        if type(data) is probe:
+            self.size = len(data.proto)
+        elif type(data) is pd.core.frame.DataFrame:
+            self.size = len(data)
+        else:
+            self.size = data
+
+
+    # Generate the target sequences
+    def generate(self):
+        seqs = []
+        LTRBeg = None
+        LTREnd = None
+        for path in self.paths:
+            with open(path) as handle:
+                seqs += list(SeqIO.parse(handle, 'fasta'))
+            if not LTRBeg:
+                LTRBeg = seqs[-1]
+            elif not LTREnd:
+                LTREnd = seqs[-1]
+        self.LTR = [LTRBeg, LTREnd]
+
+        begKmers = self.convertPAM(self.filterDegenerate(self.filterPAM(self.generateKmers(self.LTR[0]))))
+        endKmers = self.convertPAM(self.filterDegenerate(self.filterPAM(self.generateKmers(self.LTR[1]))))
+
+        self.kmers = [begKmers,endKmers]
+        return self
+
+
+    # Generate the kmers from the LTR sequnces
+    def generateKmers(self, seq):
+        rmers = Counter()
+        seqStr = str(seq.seq) #.ungap('-').upper()
+        for loc in range(len(seqStr)-self.length):
+            rmers[seqStr[loc:loc+self.length]] += 1
+        # kmers = []
+        # for mer in rmers:
+        #     if '-' not in mer:
+        #         kmers += [str(mer)]
+        return pd.Series(rmers)
+
+
+    # Filter out any seqs that do no end or start with PAM or it's compliment
+    def filterPAM(self, kmers):
+        kmers = pd.Series(kmers.index)
+        kmers = kmers[kmers.map(lambda x: x.endswith('GG') | x.startswith('CC'))]
+        kmers.reset_index()
+        return kmers
+
+
+    # Filter out any seqs that contain degenerate bases
+    def filterDegenerate(self, kmers):
+        kmers = kmers[kmers.map(lambda x: all(b not in x for b in self.degenerate))]
+        kmers.reset_index()
+        return kmers
+
+
+    # Convert any seqs starting in CC to their proper PAM seqs
+    def convertPAM(self,kmers):
+        kmers = [list(k) for k in kmers.values]
+        for i,k in enumerate(kmers):
+            l = ''.join(k)
+            if l.startswith('CC'):
+                kmers[i] = self.reverseCompliment(l)
+            else:
+                kmers[i] = l
+        return kmers
+
+
+    # Return the reverse compliment of the input sequence
+    def reverseCompliment(self, seq):
+        return ''.join([self.dictDNA[base] for base in list(seq[::-1])])
